dd2cf1c101e496cb4670ec200241f4df49978721
adnanysf <asubyusuf@gmail.com>
2025-02-15 22:59:01 -0600

    user registration added and tested

--- server/server.js
+++ server/server.js
@@ -1,13 +1,74 @@
-// server.js
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://user:dbpass@bookcluster.knhby.mongodb.net/?retryWrites=true&w=majority&appName=BookCluster";
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('hello hackers')
-  })
+async function run() {
+    try {
+      await client.connect();
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+app.get('/', async(req, res) => {
+    res.send('Hello World!');
+})
+
+app.post('/addUser', async (req, res) => {
+  const { username, password, email, name } = req.body;
+  if (!username || !password || !email || !name) {
+    return res.status(400).send('All fields are required');
+  }
+
+  try {
+    await client.connect();
+    const database = client.db('BookCluster');
+    const users = database.collection('users');
+    if (await users.findOne({ username })) {
+      return res.status(400).send('Username already exists');
+    }
+    const newUser = { username, password, email, name };
+    const result = await users.insertOne(newUser);
+    res.status(201).send(`User added with ID: ${result.insertedId}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error adding user');
+  } finally {
+    await client.close();
+  }
+});
+
+
+app.get('/users', async (req, res) => {
+  try {
+    await client.connect();
+    const database = client.db('BookCluster');
+    const users = database.collection('users');
+    const userList = await users.find({}).toArray();
+    res.status(200).json(userList);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error getting users');
+  } finally {
+    await client.close();
+  }
+
+});
